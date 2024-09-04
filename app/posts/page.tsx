2efe4f8029155cc9efdae090cbdfd05f9c53ab31
An <An@example.com>
2024-09-04 13:22:34 +0700

    test

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,11 +1,10 @@
 import Search from "@/app/components/Search";
-import { useRouter, useSearchParams } from "next/navigation";
-import React, { Suspense, useEffect, useState } from "react";
+import React from "react";
 
 export default function page() {
   return (
-    <Suspense>
-      {/* <Search></Search> */}
-    </Suspense>
+    <>
+      <Search></Search>
+    </>
   );
 }
